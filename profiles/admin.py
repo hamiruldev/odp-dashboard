f329e4a5fdd9b3d3a5f5f25168f66480d5b868b4
hamiruldev <hamirul.dev@gmail.com>
2023-12-13 23:36:33 +0800

    add import export ata profile

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 from .models import Profile
+from .resource import ReportResourceProfile
+from import_export.admin import ImportExportModelAdmin
 
 
-class ProfileAdmin(admin.ModelAdmin):
+class ProfileAdmin(ImportExportModelAdmin):
     list_display = ("user", "view_count",)
+    resource_class = ReportResourceProfile
+
 
 admin.site.register(Profile, ProfileAdmin)
